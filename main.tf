955ca4ae8aaa376e921dca22ea7b6cf5565b0ad6
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-04 19:02:36 +0530

    AWS Parameter

--- main.tf
+++ main.tf
@@ -3,38 +3,38 @@ resource "aws_docdb_subnet_group" "main" {
   subnet_ids = var.subnet_ids
   tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
 }
-#
-#resource "aws_security_group" "main" {
-#  name        = "${local.name_prefix}-sg"
-#  description = "${local.name_prefix}-sg"
-#  vpc_id      = var.vpc_id
-#  tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
-#
-#  ingress {
-#    description = "DOCDB"
-#    from_port   = 27017
-#    to_port     = 27017
-#    protocol    = "tcp"
-#    cidr_blocks = var.sg_ingress_cidr
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#}
-#
-#resource "aws_docdb_cluster_parameter_group" "main" {
-#  family      = var.engine_family
-#  name        = "${local.name_prefix}-pg"
-#  description = "${local.name_prefix}-pg"
-#  tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
-#
-#}
-#
+
+resource "aws_security_group" "main" {
+  name        = "${local.name_prefix}-sg"
+  description = "${local.name_prefix}-sg"
+  vpc_id      = var.vpc_id
+  tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
+
+  ingress {
+    description = "DOCDB"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = var.sg_ingress_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = var.engine_family
+  name        = "${local.name_prefix}-pg"
+  description = "${local.name_prefix}-pg"
+  tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
+
+}
+
 resource "aws_docdb_cluster" "main" {
   cluster_identifier              = "${local.name_prefix}-cluster"
   engine                          = "docdb"
@@ -51,11 +51,11 @@ resource "aws_docdb_cluster" "main" {
   storage_encrypted               = true
   kms_key_id                      = var.kms_key_id
 }
-#
-#
-#resource "aws_docdb_cluster_instance" "main" {
-#  count              = var.instance_count
-#  identifier         = "${local.name_prefix}-cluster-instance-${count.index + 1}"
-#  cluster_identifier = aws_docdb_cluster.main.id
-#  instance_class     = var.instance_class
-#}
+
+
+resource "aws_docdb_cluster_instance" "main" {
+  count              = var.instance_count
+  identifier         = "${local.name_prefix}-cluster-instance-${count.index + 1}"
+  cluster_identifier = aws_docdb_cluster.main.id
+  instance_class     = var.instance_class
+}
